4de09d4ad1ed98d77bbf6e698b8df0b4dc51fd75
adam <adammarey51@gmail.com>
2023-08-07 11:09:46 -0700

    inital setup - added a prompts

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>SimpleDSA</span>,
   footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    text: 'SimpleDSA'
+  }
+};
 
-export default config
+export default config;
